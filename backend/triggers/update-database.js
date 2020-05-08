69deb6440cddcfd9314640c35fbb62d56f43723e
luizmanke <luizmanke@gmail.com>
2020-05-07 22:18:06 -0300

    fix(database): Correct the update database trigger

--- backend/triggers/update-database.js
+++ backend/triggers/update-database.js
@@ -1,12 +1,21 @@
 // System libraries
 const assert = require("assert");
 const axios = require("axios");
+require("dotenv/config");
 
 describe("Update Database", async function () {
-  it("Should return status 200", function () {
+  it("Should return status 200", async function () {
+    this.timeout(600000);
     const app = process.env.HEROKU_APP;
-    url = `https://${app}.herokuapp.com/database/update`;
-    const response = await axios.post(url);
+    const response = await axios.post(
+      `https://${app}.herokuapp.com/database/update`
+    );
     assert.equal(response.status, 200);
+    while (response.status == "Out of date") {
+      await new Promise((resolve) => setTimeout(resolve, 60000));
+      response = await axios.get(
+        `https://${app}.herokuapp.com/database/status`
+      );
+    }
   });
 });
