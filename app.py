cb4908bb5d59d6717e07afea052d5e6643629bea
luizmanke <luizmanke@gmail.com>
2020-05-21 20:58:29 -0300

    build: Create triggers.py to be added to Procfile

--- app.py
+++ app.py
@@ -2,18 +2,8 @@
 # -*- coding: utf-8 -*-
 
 # System libraries
-from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 from flask_restful import Api
-
-# Own libraries
-from backend.resources.metadata import update_database
-
-# Set triggers
-scheduler = BackgroundScheduler()
-scheduler.add_job(
-    update_database, trigger="cron", day_of_week="mon-fri", hour=21, timezone="UTC")
-scheduler.start()
 
 # Initiate app
 app = Flask(__name__)
