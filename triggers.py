4bd653c3adf7ac1df0dbef2f5a19fcd2be1ca660
luizmanke <luizmanke@gmail.com>
2020-05-26 19:59:09 -0300

    fix(triggers): Change scheduler

--- triggers.py
+++ triggers.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-try:
-    # System libraries
-    from apscheduler.schedulers.background import BackgroundScheduler
 
-    # Own libraries
-    from backend.resources import daily_update
+# System libraries
+from apscheduler.schedulers.background import BlockingScheduler
 
+# Own libraries
+from backend.resources import daily_update
 
-    # Set triggers
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(
-        daily_update.run, trigger="cron", day_of_week="mon-fri", minute=15, timezone="UTC")
-    scheduler.start()
-
-except Exception as error:
-    print(error)
+# Set triggers
+scheduler = BlockingScheduler()
+scheduler.add_job(
+    daily_update.run, trigger="cron", day_of_week="mon-fri", hour=21, timezone="UTC")
+scheduler.start()
