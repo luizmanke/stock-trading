6c494ae22d8be61d7ce08d2fea065f855eefe926
luizmanke <luizmanke@gmail.com>
2020-05-26 19:09:31 -0300

    fix: Test trigger functionality

--- triggers.py
+++ triggers.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+try:
+    # System libraries
+    from apscheduler.schedulers.background import BackgroundScheduler
 
-# System libraries
-from apscheduler.schedulers.background import BackgroundScheduler
-
-# Own libraries
-from backend.resources import daily_update
+    # Own libraries
+    from backend.resources import daily_update
 
 
-# Set triggers
-scheduler = BackgroundScheduler()
-scheduler.add_job(
-    daily_update.run, trigger="cron", day_of_week="mon-fri", minute=5, timezone="UTC")
-scheduler.start()
+    # Set triggers
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(
+        daily_update.run, trigger="cron", day_of_week="mon-fri", minute=15, timezone="UTC")
+    scheduler.start()
+
+except Exception as error:
+    print(error)
