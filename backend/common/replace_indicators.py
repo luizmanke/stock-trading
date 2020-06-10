7a2154f3ef9f62e7f2aebeb7c483bcf6dadeef72
luizmanke <luizmanke@gmail.com>
2020-06-10 20:04:01 -0300

    refactor: Change folder of replace_indicators.py

--- backend/common/replace_indicators.py
+++ backend/common/replace_indicators.py
@@ -11,6 +11,7 @@ from ..analytics.strategy import Strategy
 
 
 def run():
+    print("Replacing...")
     fundamentals, quotations = _get_data()
     indicators = _compute_indicators(fundamentals, quotations)
     _delete_colection()
@@ -18,13 +19,16 @@ def run():
 
 
 def _get_data():
+
     fields = {"ticker": 1, "occurredAt": 1, "cagr": 1,
               "returnOnInvestedCapital": 1, "priceToEarnings": 1}
-    fundamentals = database.find(collection="fundamentals", projection=fields)
+    sort = [("occurredAt", 1)]
+    fundamentals = database.find(collection="fundamentals", projection=fields, sort=sort)
     fundamentals = pd.DataFrame(fundamentals)
 
     fields = {"ticker": 1, "occurredAt": 1, "close": 1, "volume": 1}
-    quotations = database.find(collection="quotations", projection=fields)
+    sort = [("occurredAt", 1)]
+    quotations = database.find(collection="quotations", projection=fields, sort=sort)
     quotations = pd.DataFrame(quotations)
 
     return fundamentals, quotations
@@ -34,10 +38,15 @@ def _compute_indicators(fundamentals, quotations):
     indicators = []
     for index in set(fundamentals["occurredAt"]):
 
-        index_fundamentals = fundamentals[fundamentals["occurredAt"] == index]
+        index_fundamentals = fundamentals[
+            (fundamentals["occurredAt"] <= index) &
+            (fundamentals["occurredAt"] > (index - dt.timedelta(days=150)))]
+        index_fundamentals = index_fundamentals.drop_duplicates(subset=["ticker"], keep="last")
+
         index_quotations = quotations[
             (quotations["occurredAt"] <= index) &
-            (quotations["occurredAt"] > (index - dt.timedelta(days=150)))]
+            (quotations["occurredAt"] > (index - dt.timedelta(days=120)))]
+
         new_indicators = Strategy().get_indicators(index_fundamentals, index_quotations)
         for item in new_indicators:
             item["occurredAt"] = index
@@ -55,5 +64,13 @@ def _delete_colection():
 
 
 def _insert_data(indicators):
-    n_inserted = database.insert(indicators, "indicators")
+    n_inserted = database.insert_many(indicators, "indicators")
     print(f" > {n_inserted} items inserted")
+
+
+if __name__ == "__main__":
+
+    # Run
+    import dotenv
+    dotenv.load_dotenv()
+    run()
